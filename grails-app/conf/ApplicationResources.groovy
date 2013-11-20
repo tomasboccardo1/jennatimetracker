0e51ec972bd61a53208364176b370f986f129f2d
federicofarina <federicofarina22@gmail.com>
2013-11-20 14:52:16 -0300

    New jquery ui version.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,12 +1,12 @@
 modules = {
     'core' {
-        resource id: 'css', url: [dir: 'css', file: 'jquery-ui-1.7.3.custom.css']
+        resource id: 'css', url: [dir: 'css', file: 'jquery-ui-1.9.2.custom.css']
         resource id: 'css', url: [dir: 'css', file: 'fg.menu.css']
         resource id: 'css', url: [dir: 'css', file: 'jquery.jgrowl.css']
         resource id: 'css', url: [dir: 'css', file: 'style.css']
 
         resource id: 'js', url: [dir: 'js', file: 'jquery/jquery-1.7.js'], disposition: 'head'
-        resource id: 'js', url: [dir: 'js', file: 'jquery-ui/jquery-ui-1.8.16.custom.js'], disposition: 'head'
+        resource id: 'js', url: [dir: 'js', file: 'jquery-ui/jquery-ui-1.9.2.custom.js'], disposition: 'head'
         resource id: 'js', url: [dir: 'js', file: 'fg.menu.js'], disposition: 'head'
         resource id: 'js', url: [dir: 'js', file: 'jquery.query-2.1.7.js'], disposition: 'head' // FIXME: Remove when paging/sorting parameters are handled correctly
         resource id: 'js', url: [dir: 'js', file: 'jgrowl/jquery.jgrowl.js'], disposition: 'head'
